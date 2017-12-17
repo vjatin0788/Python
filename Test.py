780a9c43a397c81ac655228baf2eae1125f0796d
vjatin0788 <34059084+vjatin0788@users.noreply.github.com>
2017-12-17 11:04:01 +0530

    knowledge about functions and key value

---
+++ Test.py
@@ -0,0 +1,17 @@
+    
+#array in python    
+mylist = {1,2,3}  
+ml1 = [x+1 for x in mylist]  #syntax
+
+#function/methods are objct in python
+def method1():
+    return 'hello world';
+def method2(method):
+     methodToRun = method()
+     print(methodToRun)
+#methods act like objects.
+method2(method1)
+
+#key valuue
+Map = {'a':1,'b':2}     
+Map['a']
